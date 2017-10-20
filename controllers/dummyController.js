ce93e8943ace34f156a2da7042ecfa88a087701b
lucas dos santos <lucas_rafa_94@hotmail.com>
2017-10-20 11:48:25 -0200

    commit 003

--- controllers/dummyController.js
+++ controllers/dummyController.js
@@ -10,4 +10,10 @@ module.exports = function(app) {
           console.log(JSON.stringify(req.body));
           res.send(response);
     });
+
+
+    app.post('/amilDummy/iti003', function(req, res) {
+          console.log(JSON.stringify(req.body));
+          res.send(response);
+    });
 };
