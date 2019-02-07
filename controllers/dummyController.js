8b9c1654f0dc815c6ce5b200d563572b401b1224
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2019-02-07 11:31:01 -0200

    commit

--- controllers/dummyController.js
+++ controllers/dummyController.js
@@ -13,6 +13,15 @@ module.exports = function(app) {
     res.send(response);
   });
 
+  app.get('/amilDummy/post', function(req, res) {
+    res.send(response);
+  });
+
+
+  app.get('/', function(req, res) {
+    res.send(response);
+  });
+
 
   app.post('/amilDummy/iti003', function(req, res) {
     console.log(JSON.stringify(req.body));
@@ -22,9 +31,9 @@ module.exports = function(app) {
   app.get('/ords/parceiros/dadosparceiros/rest/busca-parceiro/:parceiro', function(req, response) {
   	console.log(req.params.parceiro);
     var bodyParceiro = "";
-    
+
     bodyParceiro = req.params.parceiro;
-    
+
     var options = {
       host: '129.144.158.159',
       port: 443,
@@ -41,18 +50,18 @@ module.exports = function(app) {
         queryResponse = JSON.parse(body);
         console.log("Got a response: ", queryResponse);
         response.setHeader('Content-type', 'application/json');
-        response.send(queryResponse); 
+        response.send(queryResponse);
      });
     });
     req.end();
-    
+
   });
- 
+
   app.get('/ords/parceiros/dadosparceiros/rest/registro-utilizado/:id/proposta/:proposta/identificador/:identificador/nome/:nome/data-venda/:data/forma-pagamento/:pagamento', function(req, responseParceiros) {
 
     console.log(req.params)
     var status = 0;
-    
+
     var options = {
       host: '129.144.158.159',
       port: 443,
